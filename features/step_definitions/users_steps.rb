6f1144abdbf465cfe5a7fd454ec7c7cda1baa316
Hubert Pham <hubqwerty@berkeley.edu>
2016-10-18 12:13:59 -0700

    cucumber steps

---
+++ features/step_definitions/users_steps.rb
@@ -0,0 +1,15 @@
+Then(/^I should be able to sign up with "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be logged in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^a user exists with login "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be able to log in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
