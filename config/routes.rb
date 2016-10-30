35014a427a99a6c69fcd03021a965c55552739d4
Paige Pratt <paige.pratt@berkeley.edu>
2016-10-30 23:33:27 +0000

    community signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  resources :community 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
