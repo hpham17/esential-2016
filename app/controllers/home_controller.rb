9fb55c72b2140341f7266103e59f7ceb0e2da8cb
Julian Bacon <juliannicholasbacon@berkeley.edu>
2016-10-30 20:16:36 +0000

    Starting admin dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      render 'adminDashboard'
+      
+      @data = User.all
+    end
+      
+    #render index
   end
-
+  
+  def adminDashboard
+    if user_signed_in?
+      render 'adminDashboard'
+    end
+  end
+  
 end
