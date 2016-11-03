c1478328a8889930d2b88f22f86edfc759a52839
Hubert Pham <hubqwerty@berkeley.edu>
2016-11-03 00:03:00 -0700

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     get '/users/:id', to: 'users/sessions#show', as: 'user'
     get '/dashboard', to: 'users/sessions#dashboard'
   end
+  post '/communities/new' => 'communities#create'
   post '/images', to: 'images#create'
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   resources :communities
